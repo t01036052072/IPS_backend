6912b90935addc530a628e9edc2844be77934ec1
lamyoon815 <lamyoon815@gmail.com>
2026-05-06 20:39:34 +0900

    fix: layout 추가

--- FrontEnd/app/_layout.tsx
+++ FrontEnd/app/_layout.tsx
@@ -3,7 +3,6 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {/* (auth) 폴더 안의 화면들을 자동으로 찾아줍니다 */}
       <Stack.Screen name="(auth)/StartScreen/StartScreen" />
       <Stack.Screen name="(auth)/LoginScreen/LoginScreen" />
       <Stack.Screen name="(auth)/SignUpScreen/SignUpScreen1" />
