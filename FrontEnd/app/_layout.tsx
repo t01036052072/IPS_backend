38b2b0b8e45f874491e6132fd7d0b791179501fe
lamyoon815 <lamyoon815@gmail.com>
2026-05-14 12:34:34 +0900

    fix : 서버 주소 변경, 메인 경로 변경

--- FrontEnd/app/_layout.tsx
+++ FrontEnd/app/_layout.tsx
@@ -1,7 +1,33 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect, useState } from "react";
+import { useRootNavigationState } from "expo-router";
 
 export default function RootLayout() {
-  return (
+  const segments = useSegments();
+  const router = useRouter();
+  const navigationState = useRootNavigationState();
+
+  // 로그인 상태 (지금은 테스트용으로 false, 나중에 서버랑 연결)
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+
+  useEffect(() => {
+  
+    if (!navigationState?.key) return;
+
+    const timeout = setTimeout(() => {
+      const inTabsGroup = segments[0] === "(tabs)";
+
+      if (!isLoggedIn && inTabsGroup) {
+        router.replace("/(auth)/StartScreen/StartScreen");
+      } else if (isLoggedIn && !inTabsGroup) {
+        router.replace("/(tabs)");
+      }
+    }, 1);
+
+    return () => clearTimeout(timeout);
+  }, [isLoggedIn, segments, navigationState?.key]);
+
+    return (
     <Stack screenOptions={{ headerShown: false }}>
 
       {/* auth */}
